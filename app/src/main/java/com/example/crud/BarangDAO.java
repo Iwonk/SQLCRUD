f47b5ff1075362473f3708202c6213b5ee023679
iwonk <i.wonk100@gmail.com>
2019-11-14 16:32:26 +0700

    wkowkowk

---
+++ app/src/main/java/com/example/crud/BarangDAO.java
@@ -0,0 +1,24 @@
+package com.example.crud;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface BarangDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insertBarang(Barang barang);
+
+    @Update
+    int updateBarang(Barang barang);
+
+    @Delete
+    int deleteBarang(Barang barang);
+
+    @Query("SELECT * FROM tbarang")
+    Barang[] selectAllBarangs();
+
+}
